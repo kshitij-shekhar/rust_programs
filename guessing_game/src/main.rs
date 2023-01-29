90c0a002c9f5ab7a443d2914531bdc795e0e23d3
kshitij-shekhar <k.shekhar219@gmail.com>
2023-01-29 18:32:38 +0530

    Updated main.rs

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,9 @@
-use rand::Rng;
+use rand::Rng;//Rng trait brings into scope functions needed to generate Random numbers
 use std::io;
 fn main() {
     println!("Guess the number!");
+    let secret_number=rand::thread_rng().gen_range(1..=10);
+    println!("The secret number is : {secret_number}");
     println!("Please enter your guess!");
     let mut guess = String::new();
     io::stdin()
